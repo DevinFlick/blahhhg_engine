9d2319baf3666d4b7c32aa305f1417ff5ebe9cc7
Justin Dennison <justin1dennison@gmail.com>
2016-08-22 19:20:24 -0400

    added comments for the backend routes that were added

--- routes/posts.js
+++ routes/posts.js
@@ -88,6 +88,13 @@ function updatePost(req, res, next){
       }
     });
 }
+
+
+/**
+ * The purpose of this route handler is to provide the ability to query the database
+ * for a :number of recent blog posts based on ordering by date.
+ * URL: /posts/recent/:number where :number would be the number that you want to request
+ */
 function getTheMostRecentNumberOfPosts(req, res, next){
   Post.find({}, function(err, posts){
     if(err){
@@ -95,15 +102,38 @@ function getTheMostRecentNumberOfPosts(req, res, next){
         msg: err
       });
     } else {
-      var numberOfPostsToGet = req.params.number;
+      var numberOfPostsToGet = req.params.number; //grabbing the number from the url
+      //The following variables uses lodash to order the posts array by the
+      // create tag in ascending order. Remember the posts variables would be
+      // an array that has objects in it that looks like the following:
+      // {
+      //  title: 'Awesome Post',
+      //  body: "I have done some amazing things!",
+      //  created: 2016-08-01T14:00:00Z
+      //  author: 74iify4i82edujfk,
+      //  updated: 2016-08-04T18:00:00Z
+      // }
       var postsOrderedByDate = _.orderBy(posts, ['created'], ['asc']);
+      // The following function takes the previous array of values and
+      // grabs the first :number (for example 10) of posts of the ordered posts
       var firstNumberOfOrderedPosts = _.take(postsOrderedByDate, numberOfPostsToGet);
+      //Return the first :number of date ordered posts
       res.status(200).json({
         posts: firstNumberOfOrderedPosts
       });
     }
   });
 }
+
+
+/**
+ * getRandomNumberOfPosts - description
+ *  The purpose of this function is to obtain all of the posts out of the database
+ *  then randomize those posts. After randomizing those posts, then you will 'grab'
+ *  the first :number of posts from the random posts.
+ *  URL: /posts/random/:number where :number would be the number of posts that
+ *    you requested
+ */
 function getRandomNumberOfPosts(req, res, next){
   Post.find({}, function(err, posts){
     if(err){
@@ -111,8 +141,12 @@ function getRandomNumberOfPosts(req, res, next){
         msg: err
       });
     } else {
-      var numOfPostsToGet = req.params.number;
+
+      var numOfPostsToGet = req.params.number; //grabbing the number from the url
+      // The following variable uses lodash to randomize the array output
       var randomizedPosts = _.shuffle(posts);
+      //The following function grabs the first :number of posts from the
+      // randomized posts array
       var firstNumberOfRandomizedPosts = _.take(randomizedPosts, numOfPostsToGet);
       res.status(200).json({
         posts: firstNumberOfRandomizedPosts
